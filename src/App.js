87c3d04c603286e47141b3653f7a9589ad5c8c8e
laxman <lxmn2010@gmail.com>
2020-10-23 03:15:58 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import TableHeader from "./components/TableHeader";
 import TableBody from "./components/TableBody";
@@ -8,28 +8,34 @@ import updateBooklist from "./actions/actions";
 import styles from "./App.module.scss";
 
 const App = () => {
-  const { bookLists } = useSelector(state => state);
-  console.log('bookLists', bookLists);
+  const { bookLists } = useSelector((state) => state);
+  console.log("bookLists", bookLists);
   const dispatch = useDispatch();
-
+  const [isLoading, setIsLoading] = useState(true);
 
   const getBookList = (url) => {
     axios.get(url).then((res) => {
       dispatch(updateBooklist(res.data));
+      setIsLoading(false);
     });
   };
 
   useEffect(() => {
+    setIsLoading(true);
     getBookList(bookListsApi);
   }, []);
 
-  if(bookLists.length <= 0){
-    return <div><strong>Loading...</strong></div>;
+  if (isLoading && bookLists.length <= 0) {
+    return (
+      <div className={styles.loader}>
+        <h2>Loading...</h2>
+      </div>
+    );
   }
-  
+
   return (
     <div>
-      <h1 className={styles.title}>React Dynamic Table</h1>
+      <h1 className={styles.title}>Lists of All Book</h1>
       <table className={styles.bookLists}>
         <tbody>
           <tr>
